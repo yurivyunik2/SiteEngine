be977daefc1b8224912ca275fbb3fdd6bcc2d94f
yurivyunik2 <jurij_vyunik2@mail.ru>
2016-02-06 13:05:24 +0200

    no message

--- Client/js/components/LayoutContentCtrl/LayoutContentCtrl.js
+++ Client/js/components/LayoutContentCtrl/LayoutContentCtrl.js
@@ -9,6 +9,8 @@ define(["application", "CONST", "Utils", "CommonTypes"], function (application, 
   var pathTemplate = "/SiteEngine/Client/js/components/LayoutContentCtrl/LayoutContentCtrl.html";
 
   var dvRemoveButtonSelector = ".dvRemoveButton";
+  var dvEditButtonSelector = ".dvEditButton";
+  var dvOkButtonSelector = ".dvOkButton";
 
   var arUserAgents = [];
 
@@ -46,9 +48,11 @@ define(["application", "CONST", "Utils", "CommonTypes"], function (application, 
               userAgent: $dvEnterFieldsElem.find(".inUserAgent").val(),
               path: $dvEnterFieldsElem.find(".inPath").val(),
             };
-            self.addUserAgent(userAgent);
-            $dvEnterFieldsElem.find(".inUserAgent").val("");
-            $dvEnterFieldsElem.find(".inPath").val("");
+            var isAdded = self.addUserAgent(userAgent);
+            if (isAdded) {
+              $dvEnterFieldsElem.find(".inUserAgent").val("");
+              $dvEnterFieldsElem.find(".inPath").val("");
+            }
           });
         }
       },
@@ -78,8 +82,6 @@ define(["application", "CONST", "Utils", "CommonTypes"], function (application, 
               _.each(userAgents, function (userAgent) {
                 self.addUserAgent(userAgent);
               });
-
-              $el.find(dvRemoveButtonSelector).click(self.removeClick);
             }
           } catch (ex) {
           }
@@ -88,15 +90,28 @@ define(["application", "CONST", "Utils", "CommonTypes"], function (application, 
       },
 
       addUserAgent: function (userAgent) {
-        if (!userAgent)
+        if (!userAgent || !userAgent.path)
+          return;
+
+        var userAgentFound = _.find(arUserAgents, { userAgent: userAgent.userAgent });
+        if (userAgentFound)
           return;
 
         arUserAgents.push(userAgent);
 
-        var htmlUserAgent = $layoutContentTemplateObj({ item: userAgent });
-
         var $dvCurrentFieldsElem = $el.find(".dvCurrentFields");
+        var htmlUserAgent = $layoutContentTemplateObj({ item: userAgent });        
         $dvCurrentFieldsElem.prepend(htmlUserAgent);
+        var elemAdded = $dvCurrentFieldsElem.children().first();
+        elemAdded.find(dvRemoveButtonSelector).click(self.removeClick);
+        elemAdded.find(dvEditButtonSelector).click(self.editClick);
+        elemAdded.find("input").keydown(function (event) {
+          event = event || window.event;
+          if (event.which === CONST.ENTER_KEY()) {
+            self.okClick(event);
+          }
+        });
+        elemAdded.find(dvOkButtonSelector).click(self.okClick);
       },
 
       removeClick: function (event) {
@@ -107,7 +122,49 @@ define(["application", "CONST", "Utils", "CommonTypes"], function (application, 
         if (userAgentFound) {
           arUserAgents = _.without(arUserAgents, userAgentFound);
           $dvUserAgentPathElem.remove();
-        }        
+        }
+      },
+
+      editClick: function (event) {
+        self.setEditMode(true);
+      },
+
+      setEditMode: function (isEdit) {
+        var $dvUserAgentPathElem = $(event.currentTarget).parents(".dvUserAgentPath");
+
+        var spanDisplay = isEdit ? "none" : "inline-block";
+        var inputDisplay = isEdit ? "block" : "none";
+
+        var $spUserAgentValueElem = $dvUserAgentPathElem.find(".spUserAgentValue");
+        var $inUserAgentElem = $dvUserAgentPathElem.find(".inUserAgent");
+        $inUserAgentElem.val($spUserAgentValueElem.html());
+        //$spUserAgentValueElem.css("display", spanDisplay);
+        //$inUserAgentElem.css("display", inputDisplay);
+
+        var $spPathValueElem = $dvUserAgentPathElem.find(".spPathValue");
+        var $inPathElem = $dvUserAgentPathElem.find(".inPath");
+        $inPathElem.val($spPathValueElem.html());
+        $spPathValueElem.css("display", spanDisplay);
+        $inPathElem.css("display", inputDisplay);
+
+        $dvUserAgentPathElem.find(".dvIcon").css("display", spanDisplay);
+        $dvUserAgentPathElem.find(".dvOkButton").css("display", inputDisplay);
+      },
+
+      okClick: function (event) {        
+        var $dvUserAgentPathElem = $(event.currentTarget).parents(".dvUserAgentPath");
+        var $spUserAgentValueElem = $dvUserAgentPathElem.find(".spUserAgentValue");
+
+        var $spPathValueElem = $dvUserAgentPathElem.find(".spPathValue");
+        var $inPathElem = $dvUserAgentPathElem.find(".inPath");
+
+        var userAgentFound = _.find(arUserAgents, { userAgent: $spUserAgentValueElem.html() })
+        if (userAgentFound) {
+          userAgentFound.path = $inPathElem.val();
+          $spPathValueElem.html($inPathElem.val());
+        }
+
+        self.setEditMode(false);
       },
 
     });
